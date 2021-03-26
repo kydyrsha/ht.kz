205d3a188d91de88609c2fada8941d40d54f4c70
kydyrsha <sanatkydyrsha@gmail.com>
2021-03-26 16:28:05 +0600

    only html added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
